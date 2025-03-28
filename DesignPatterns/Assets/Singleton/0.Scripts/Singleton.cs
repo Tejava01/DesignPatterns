b9c6857130fe6b8aef2454c8bbc6ed43b781a6d1
Tejava01 <hewk0528@naver.com>
2025-03-28 18:50:48 +0900

    싱글톤 구현

---
+++ DesignPatterns/Assets/Singleton/0.Scripts/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+    public static T Instance
+    {
+        get { return instance; }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            Debug.Log(gameObject.name+" 생성");
+        }
+        else
+        {
+            Destroy(gameObject);
+            Debug.Log(instance.gameObject.name+" 인스턴스가 존재하여 "+gameObject.name + " 파괴");
+        }
+    }
+
+}
